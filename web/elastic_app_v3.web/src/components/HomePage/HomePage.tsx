6d97b5ab11c32e8efe74a2d2069b1d3695936a3b
alexander-player-cko <alexander.player@checkout.com>
2026-06-14 16:35:07 +0100

    add some aesthetic to the home page with test img

--- web/elastic_app_v3.web/src/components/HomePage/HomePage.tsx
+++ web/elastic_app_v3.web/src/components/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { User } from 'lucide-react';
 import styles from './HomePage.module.css';
 import { useNavigate } from 'react-router-dom';
+import subnautica from '../../assets/subnautica.jpg'
 import { paths } from '../../routes/paths';
 const HomePage = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,21 +10,22 @@ const HomePage = () => {
     
     return (
         <div>
-            <div className={styles.profileButton}>
-                <button onClick={() => setIsOpen(!isOpen)}>
-                    <User size={24} />
-                </button>
-                {isOpen && (
-                    <div className={styles.dropdown}>
-                        <ul>
-                            <li onClick={() => navigate(paths.settings)}>Settings</li>
-                        </ul>
-                    </div>
-                )}
+            <div className={styles.topRibbon}>
+                <div className={styles.profileButton}>
+                    <button onClick={() => setIsOpen(!isOpen)}>
+                        <User size={24} />
+                    </button>
+                    {isOpen && (
+                        <div className={styles.dropdown}>
+                            <ul>
+                                <li onClick={() => navigate(paths.settings)}>Settings</li>
+                            </ul>
+                        </div>
+                    )}
+                </div>
             </div>
-            <div className={styles.container}>
-                <h1>Welcome Home!</h1>
-                <p id="message"></p>
+            <div className={styles.hero}>
+                <h1 className={styles.sloganText}>Get Out There!</h1>
             </div>
         </div>
     );
